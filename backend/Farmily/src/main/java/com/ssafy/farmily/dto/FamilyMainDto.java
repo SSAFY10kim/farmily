182691b2ba7addc6e9b455e3e9599f9154f9dfd4
구본웅 <thesmartist3@gmail.com>
2024-01-24 17:28:56 +0900

    refactor: 패키지 구조

--- backend/Farmily/src/main/java/com/ssafy/farmily/dto/FamilyMainDto.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/dto/FamilyMainDto.java
@@ -3,14 +3,14 @@ package com.ssafy.farmily.dto;
 import java.util.List;
 
 import com.ssafy.farmily.entity.Family;
+import com.ssafy.farmily.entity.Tree;
 
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
-public class
-FamilyMainDto {
+public class FamilyMainDto {
 	Long id;
 	String name;
 	String motto;
@@ -22,6 +22,8 @@ FamilyMainDto {
 		familyMainDTO.setId(family.getId());
 		familyMainDTO.setName(family.getName());
 		familyMainDTO.setMotto(family.getMotto());
+		Tree tree = family.getTree();
+		familyMainDTO.setTree(FamilyMainTreeDto.from(tree));
 		return familyMainDTO;
 	}
 
